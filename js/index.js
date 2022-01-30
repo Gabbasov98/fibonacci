b41a33d10c7590fc5861d0fe574f2587befc8cc9
Marsel Gabbasov <marsel13_98@mail.ru>
2022-01-31 01:32:20 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,37 @@
+function slider() {
+    var swiper = new Swiper('.feedback .swiper-container', {
+        slidesPerView: 5,
+        spaceBetween: 0,
+        centeredSlides: true,
+        loop: true,
+        navigation: {
+            nextEl: '.feedback .swiper-button-next',
+            prevEl: '.feedback .swiper-button-prev',
+        },
+        pagination: {
+            el: '.feedback .swiper-pagination',
+            type: 'bullets',
+            clickable: true,
+        },
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+                centeredSlides: false,
+            },
+            992: {
+                slidesPerView: 5,
+                centeredSlides: true,
+            },
+        }
+    })
+}
+
+$(document).ready(function() {
+    $(".tab").click(function() {
+        let path = $(this).attr("data-tab-path")
+        $(".tab").removeClass("tab--active")
+        $(this).addClass("tab--active")
+        $(".tab-content").removeClass("tab-content--active")
+        $(`.tab-content[data-tab-path="${path}"]`).addClass("tab-content--active")
+    })
+})
