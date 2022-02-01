98b702eb5e143088bc4178c967618c8b6be5f7aa
Marsel Gabbasov <marsel13_98@mail.ru>
2022-02-01 10:10:53 +0300

    inputs

--- js/index.js
+++ js/index.js
@@ -34,4 +34,15 @@ $(document).ready(function() {
         $(".tab-content").removeClass("tab-content--active")
         $(`.tab-content[data-tab-path="${path}"]`).addClass("tab-content--active")
     })
+
+    function validate(evt) {
+        var theEvent = evt || window.event;
+        var key = theEvent.keyCode || theEvent.which;
+        key = String.fromCharCode(key);
+        var regex = /[0-9]|\./;
+        if (!regex.test(key)) {
+            theEvent.returnValue = false;
+            if (theEvent.preventDefault) theEvent.preventDefault();
+        }
+    }
 })
