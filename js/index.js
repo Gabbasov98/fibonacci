ca6e2bf37cad0d0f907640332e53408de21c5726
Marsel Gabbasov <marsel13_98@mail.ru>
2022-02-01 10:14:05 +0300

    inputs

--- js/index.js
+++ js/index.js
@@ -35,7 +35,18 @@ $(document).ready(function() {
         $(`.tab-content[data-tab-path="${path}"]`).addClass("tab-content--active")
     })
 
-    function validate(evt) {
+    // function validate(evt) {
+    //     var theEvent = evt || window.event;
+    //     var key = theEvent.keyCode || theEvent.which;
+    //     key = String.fromCharCode(key);
+    //     var regex = /[0-9]|\./;
+    //     if (!regex.test(key)) {
+    //         theEvent.returnValue = false;
+    //         if (theEvent.preventDefault) theEvent.preventDefault();
+    //     }
+    // }
+
+    $(".modal__form-group--min input").keypress(function(evt) {
         var theEvent = evt || window.event;
         var key = theEvent.keyCode || theEvent.which;
         key = String.fromCharCode(key);
@@ -44,5 +55,5 @@ $(document).ready(function() {
             theEvent.returnValue = false;
             if (theEvent.preventDefault) theEvent.preventDefault();
         }
-    }
+    })
 })
